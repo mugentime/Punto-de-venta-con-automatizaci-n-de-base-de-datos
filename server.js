357cbfd151cd2e34b1a086c8b1e2394ab5f6c69a
mugentime <je2alvarela@gmail.com>
2025-08-20 06:55:38 -0600

    ssss

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet({
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
       imgSrc: ["'self'", "data:", "https:"],
-      connectSrc: ["'self'"]
+      connectSrc: ["'self'", "https://cdnjs.cloudflare.com", "https://cdn.jsdelivr.net"]
     }
   }
 }));
